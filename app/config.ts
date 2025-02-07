bc69588b9161170dd226affb729d56efafdfd228
clothme <57736523+clothme-io@users.noreply.github.com>
2025-02-07 15:48:19 -0800

    chore: update env values

--- app/config.ts
+++ app/config.ts
@@ -4,22 +4,34 @@ import { getAnalytics, isSupported } from "firebase/analytics";
 // Supabase
 import { createClient } from '@supabase/supabase-js'
 
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+// };
+
+// const firebaseCValues = JSON.stringify(process.env.NEXT_PUBLIC_FIREBASE || {});
+const firebaseEnvString = process.env.NEXT_PUBLIC_FIREBASE_STRING as string;
+const myEnvArray = firebaseEnvString.split("|");
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: myEnvArray[0],
+  authDomain: myEnvArray[1],
+  projectId: myEnvArray[2],
+  storageBucket: myEnvArray[3],
+  messagingSenderId: myEnvArray[4],
+  appId: myEnvArray[5],
+  measurementId: myEnvArray[6]
 };
 
 // Initialize Firebase
+// export const app = initializeApp(firebaseCValues as unknown as object);
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const analytics = isSupported().then(yes => yes ? getAnalytics(app) : null);
-
-
 
 // Supabse Client
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
