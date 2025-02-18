49176cabffb0f6f95cf5d4c54e765e2177ac60ff
clothme <paul.ikhane@outlook.com>
2025-02-17 17:58:53 -0800

    chore: updated state

--- app/platform/project/(projectList)/page.tsx
+++ app/platform/project/(projectList)/page.tsx
@@ -1,7 +1,29 @@
+"use client"
+
+import React, { useState } from "react"
 import { ProjectTable } from "./components/project-table"
 import { ProjectHeader } from "./components/project-header"
 
+export interface ProjectPageProps {
+    streetNumber: string;
+    streetName: string;
+    city: string;
+    state: string;
+    zip: string;
+    country: string;
+}
+
 export default function ProjectPage() {
+
+    const [propertyAddress, setPropertyAddress] = useState<ProjectPageProps>({
+        streetNumber: '',
+        streetName: '',
+        city: '',
+        state: '',
+        zip: '',
+        country: ''
+    });
+
     return (
         <div className="flex flex-col gap-6 py-16 px-48">
             <ProjectHeader />
