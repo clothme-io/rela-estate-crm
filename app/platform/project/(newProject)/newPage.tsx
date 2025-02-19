477e66356b8a203feda4440f209f412ad58fb9c6
clothme <paul.ikhane@outlook.com>
2025-02-18 20:05:08 -0800

    chore: added address api

--- app/platform/project/(newProject)/newPage.tsx
+++ app/platform/project/(newProject)/newPage.tsx
@@ -1,8 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client"
 
+import {
+    toast
+} from "sonner"
+import {
+    useForm
+} from "react-hook-form"
 import React, { useEffect, useState, useRef } from "react"
 import { useLoadScript } from "@react-google-maps/api"
+import {
+    zodResolver
+} from "@hookform/resolvers/zod"
+import * as z from "zod"
 import { Button } from "@/components/ui/button"
 import {
     Dialog,
@@ -16,11 +26,49 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Plus } from "lucide-react"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+
+const formSchema = z.object({
+    name_0296298989: z.string(),
+    name_4959118455: z.string(),
+    name_1263799749: z.string(),
+    name_7929842234: z.string(),
+    name_4642882506: z.string(),
+    name_3920761924: z.string()
+});
 
 export function NewProjectDialog() {
 
-    const [input, setIput] = useState({});
+    const [input, setInput] = useState({});
+    const [notLoaded, setLoaded] = useState(true);
     const inputRef = useRef<HTMLInputElement>(null);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        try {
+            console.log(values);
+            toast(
+                <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+                    <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+                </pre>
+            );
+        } catch (error) {
+            console.error("Form submission error", error);
+            toast.error("Failed to submit the form. Please try again.");
+        }
+    }
 
     const libraries: any = ["places"]
     const { isLoaded, loadError } = useLoadScript({
@@ -30,22 +78,22 @@ export function NewProjectDialog() {
 
     const handleChange = (event: any) => {
         const { name, value } = event.target;
-        setIput((values) => ({ ...values, [name]: value }));
+        setInput((values) => ({ ...values, [name]: value }));
     }
 
-    const handlePlaceChanged = async (address) => {
+    const handlePlaceChanged = async (address: any) => {
         if (!isLoaded) return;
         const place = address.getPlace();
 
         if (!place || !place.geometry) {
-            setIput({});
+            setInput({});
             return;
         }
 
         formData(place);
     }
 
-    const formData = (data) => {
+    const formData = (data: { address_components: any; geometry: { location: { lat: () => any; lng: () => any } } }) => {
         const addressComponents = data?.address_components;
 
         const componentMap = {
@@ -66,7 +114,21 @@ export function NewProjectDialog() {
             }
         }
 
-        const formattedAddress = `${componentMap.street_number} ${componentMap.route}`;
+        const formattedAddress = `${componentMap.subPremise} ${componentMap.premise} ${componentMap.street_number} ${componentMap.route}`.trim();
+
+        const latitude = data?.geometry?.location?.lat();
+        const longitude = data?.geometry?.location?.lng();
+
+        setInput((values: any) => ({
+            ...values,
+            streetAddress: formattedAddress,
+            country: componentMap.country,
+            zipCode: componentMap.postal_code,
+            city: componentMap.administrative_area_level_2,
+            state: componentMap.administrative_area_level_1,
+            latitude,
+            longitude,
+        }));
     }
 
     useEffect(() => {
@@ -78,10 +140,10 @@ export function NewProjectDialog() {
             }
             if (inputRef.current) {
                 const autocomplete = new google.maps.places.Autocomplete(inputRef.current, options);
-                autocomplete.addListener("place_changed", () => handleChange(autocomplete));
+                autocomplete.addListener("place_changed", () => handlePlaceChanged(autocomplete));
             }
         }
-    }, [isLoaded, loadError]);
+    }, [handlePlaceChanged, isLoaded, loadError]);
 
     return (
         <Dialog>
@@ -107,9 +169,148 @@ export function NewProjectDialog() {
                         </Label>
                     </div>
                     <div className="grid grid-cols-2 items-center gap-4">
-                        <Input id="username" value="@peduarte" className="col-span-3" />
+                        <Input id="username" value="" className="col-span-3" />
                     </div>
                 </div>
+
+                {notLoaded && (
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2 max-w-3xl mx-auto py-4">
+
+                            <div className="grid grid-cols-12 gap-4">
+
+                                <div className="col-span-6">
+
+                                    <FormField
+                                        control={form.control}
+                                        name="name_0296298989"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Street Address</FormLabel>
+                                                <FormControl>
+                                                    <Input
+                                                        placeholder=""
+                                                        disabled
+                                                        type=""
+                                                        {...field} />
+                                                </FormControl>
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+
+                                <div className="col-span-6">
+
+                                    <FormField
+                                        control={form.control}
+                                        name="name_4959118455"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Alternative Street Address</FormLabel>
+                                                <FormControl>
+                                                    <Input
+                                                        placeholder=""
+                                                        disabled
+                                                        type=""
+                                                        {...field} />
+                                                </FormControl>
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+
+                            </div>
+
+                            <div className="grid grid-cols-12 gap-4">
+
+                                <div className="col-span-6">
+
+                                    <FormField
+                                        control={form.control}
+                                        name="name_1263799749"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>City</FormLabel>
+                                                <FormControl>
+                                                    <Input
+                                                        placeholder=""
+                                                        disabled
+                                                        type=""
+                                                        {...field} />
+                                                </FormControl>
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+
+                                <div className="col-span-6">
+
+                                    <FormField
+                                        control={form.control}
+                                        name="name_7929842234"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>State</FormLabel>
+                                                <FormControl>
+                                                    <Input
+                                                        placeholder=""
+                                                        disabled
+                                                        type=""
+                                                        {...field} />
+                                                </FormControl>
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+
+                            </div>
+
+                            <div className="grid grid-cols-12 gap-4">
+
+                                <div className="col-span-6">
+
+                                    <FormField
+                                        control={form.control}
+                                        name="name_4642882506"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Country</FormLabel>
+                                                <FormControl>
+                                                    <Input
+                                                        placeholder=""
+                                                        disabled
+                                                        type=""
+                                                        {...field} />
+                                                </FormControl>
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+
+                                <div className="col-span-6">
+
+                                    <FormField
+                                        control={form.control}
+                                        name="name_3920761924"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Zip Code</FormLabel>
+                                                <FormControl>
+                                                    <Input
+                                                        placeholder=""
+                                                        disabled
+                                                        type=""
+                                                        {...field} />
+                                                </FormControl>
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+
+                            </div>
+                        </form>
+                    </Form>
+                )}
                 <DialogFooter>
                     <Button type="submit">Sumbit</Button>
                 </DialogFooter>
